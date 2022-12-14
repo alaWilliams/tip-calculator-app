07ec7a4071c9932df93027abd36147fd4dd57150
Ala Williams <alicja.m.williams@gmail.com>
2022-12-14 15:10:31 +0100

    initial input handlers

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const billTotal = document.querySelector('.bill-amount');
-const numOfPeople = document.querySelector(".people-amount")
+const billInput = document.querySelector('.bill-amount');
+const numOfPeople = document.querySelector('.people-amount');
 
+const split = {
+	total: 0,
+	tip: 0,
+	people: 0,
+};
 
+billInput.addEventListener('change', () => {
+	split.total = Number(billInput.value);
+});
+
+numOfPeople.addEventListener('change', () => {
+  split.people = Number(numOfPeople.value);
+})
+
+const five = document.querySelector('.btn');
+five.addEventListener('click', () => {
+  split.tip = Number(five.value);
+  console.log(split);
+})
+
+const handleClick = (btn) => {
+  split.tip = Number(btn.value);
+}
